763e7780cd1495abdf4b07aea665a8b1de0445a1
ShinJiHyeong <bs9992@nara.co.kr>
2021-06-08 16:46:07 +0900

    Initial commit

---
+++ src/main/java/org/example/miyabayt/MiyabaytApplication.java
@@ -0,0 +1,12 @@
+package org.example.miyabayt;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiyabaytApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MiyabaytApplication.class);
+	}
+}
